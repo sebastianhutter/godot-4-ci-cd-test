7ec06a8f188d1779880a5f3a8333f04a9f669851
Sebastian Hutter <mail@sebastian-hutter.ch>
2023-12-24 09:30:48 +0100

    Add ui manager

---
+++ scenes/manager/ui_manager/ui_manager.gd
@@ -0,0 +1,59 @@
+extends Manager
+class_name UiManager
+
+# ========
+# singleton references
+# ========
+
+# ========
+# export vars
+# ========
+
+# ========
+# class signals
+# ========
+
+# ========
+# class onready vars
+# ========
+
+# ========
+# class vars
+# ========
+
+# ========
+# godot functions
+# ========
+
+func _ready() -> void:
+	_hide_all_ui_elements() 
+
+# ========
+# signal handler
+# ========
+
+# ========
+# class functions
+# ========
+
+func _transition_to_game_loop() -> void:
+	_show_all_ui_elements() 
+	
+func _transition_to_menu() -> void:
+	_hide_all_ui_elements() 
+
+func _transition_to_pause() -> void:
+	_hide_all_ui_elements() 
+	
+func _transition_to_game_loop_from_pause_menu() -> void:
+	_show_all_ui_elements()
+
+func _hide_all_ui_elements() -> void:
+	for child in get_children():
+		child.hide()
+		
+func _show_all_ui_elements() -> void:
+	for child in get_children():
+		child.show()
+
+
